f2500ebbcd856a31f4c3a634e7e4e696ac33c688
Gabriel <bristotgl@gmail.com>
2023-03-27 21:42:45 -0300

    adds default profile

--- src/main/java/siegjor/simpleproject/services/i18n/EnglishGreetingService.java
+++ src/main/java/siegjor/simpleproject/services/i18n/EnglishGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import siegjor.simpleproject.services.GreetingService;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18NService")
 public class EnglishGreetingService implements GreetingService {
 
